3f42fc541ec888462a65e5c7e67a1cdc13ccf3e1
root <root@ip-172-31-25-246.ec2.internal>
2021-05-17 20:44:00 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import subprocess
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app=Flask("myapp")
+app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///data/data.sqlite"
+db=SQLAlchemy(app)
+class Users(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    uname=db.Column(db.Text)
+    email=db.Column(db.Text)
+    passwd=db.Column(db.Text)
+
+    def __init__(self, uname, email, passwd):
+        self.uname=uname
+        self.email=email
+        self.passwd=passwd
+
+db.create_all()
+
+@app.route("/")
+def home():
+    return render_template("start.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/index", methods=["POST"])
+def index():
+    cmd=cmd = "sudo docker ps --all"
+    output = subprocess.getoutput(cmd)
+    container_list = output.split("\n")
+    uname=request.form.get("new-uname")
+    email=request.form.get("new-mail")
+    psw=request.form.get("new-psw")
+    return render_template("console.html", c_list=container_list)
+
+
+@app.route("/reset", methods=["POST"])
+def pass_reset():
+    return render_template("password-reset.html")
+
+
+@app.route("/launch", methods=["POST"])
+def doc_start():
+    c_name=request.form.get("x")
+    x=subprocess.getoutput("sudo docker run -d -i -t  --name {} party:latest".format(cname))
+    ip_addr=subprocess.getoutput("sudo docker inspect --format '{{.NetworkSettings.IPAddress}}' {}".format(cname))
+    y=subprocess.getoutput("sudo docker exec -d {} sed -i 's/lakshyagupta/{}/' /etc/sysconfig/shellinaboxd".format(c_name,ip_addr))
+    z=subprocess.getoutput("sudo docker exec -d {} /usr/sbin/shellinaboxd --disable-ssl -b".format(c_name))
+    return redirect(url_for('doc_home'))
+
+app.run(host='0.0.0.0', port=80, debug=True)
+
+
